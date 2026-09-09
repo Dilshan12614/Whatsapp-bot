9ddb842f73ef0e04d3ea4170244543d3f28995b4
Dilshan12614 <dilshanashinsa793@gmail.com>
2026-09-09 22:15:29 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+const PREFIX = ".";
+const BOT_NAME = "DILA-MD";
+const OWNER_NAME = "Dilshan";
+const OWNER_NUMBER = "947XXXXXXXX";
+
+const MODE = "public";
+const AUTO_READ = false;
+const AUTO_TYPING = false;
+
+const SESSION_ID = process.env.SESSION_ID || "";
+
+module.exports = {
+  PREFIX,
+  BOT_NAME,
+  OWNER_NAME,
+  OWNER_NUMBER,
+  MODE,
+  AUTO_READ,
+  AUTO_TYPING,
+  SESSION_ID
+};
