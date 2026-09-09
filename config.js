66470e7c21892d5719093b6462d77249a385aeb0
Dilshan12614 <dilshanashinsa793@gmail.com>
2026-09-09 22:19:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const PREFIX = ".";
 const BOT_NAME = "DILA-MD";
 const OWNER_NAME = "Dilshan";
-const OWNER_NUMBER = "947XXXXXXXX";
+const OWNER_NUMBER = "94740534738";
 
 const MODE = "public";
 const AUTO_READ = false;
 const AUTO_TYPING = false;
 
-const SESSION_ID = process.env.SESSION_ID || "";
+const SESSION_ID = process.env.SESSION_ID || "8Q9wASxS#z-8xtaeQejj0230xr_KXB_Fg4noAz6ZhVgDwUcdAlJw";
 
 module.exports = {
   PREFIX,
