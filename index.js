37a9cbde0fd0b5968eb7587685ebb2b6f950bf06
Dilshan12614 <dilshanashinsa793@gmail.com>
2026-09-10 07:49:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {
   useMultiFileAuthState,
   fetchLatestBaileysVersion,
   DisconnectReason
-} = require("@whiskeysockets/baileys");
+} = require('baileys-pro');
 
 const pino = require("pino");
 const config = require("./config");
